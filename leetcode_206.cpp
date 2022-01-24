bb056c85d83295e5b8155f83bec5a2e8b8f4acd1
Shivani Kushwaha <30427045+shivanikush@users.noreply.github.com>
2022-01-24 18:08:00 +0530

    Create leetcode_206.cpp

---
+++ leetcode_206.cpp
@@ -0,0 +1,16 @@
+ListNode* reverseList(ListNode* head) {
+        ListNode *prev = NULL;
+        ListNode *curr = head;
+        ListNode *n;
+        
+        while(curr!=NULL){
+            n = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = n;
+            
+        }
+        return prev;
+        
+        
+    }
