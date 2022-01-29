f93cb3e13e4f35e7458d0f09656cc30ffa8a8540
Shivani Kushwaha <30427045+shivanikush@users.noreply.github.com>
2022-01-29 16:50:48 +0530

    Create leetcode_226.cpp

---
+++ leetcode_226.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(!root) return root;
+        TreeNode* right = root->right;
+        root->right = invertTree(root->left);
+        root->left = invertTree(right);
+        return root;
+    }
+};
